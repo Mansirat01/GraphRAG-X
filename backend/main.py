c7ceb1a65b2093c10323f0a5f95e015fa68f966f
Mansirat Kaur <mansiratsidhu2840@gmail.com>
2026-02-21 11:29:46 +0530

    Step 1 : Uploading a Document is done.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def root():
     return {"message": "GraphRAG-X backend running"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"filename": file.filename, "status": "uploaded successfully"}
